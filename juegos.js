b040bd25a0216f4b2dca259f6ccd714e939c6235
Martin Bustamante <martin.m.bustamante@gmail.com>
2022-12-11 01:29:55 -0300

    creacion de array de objetos

---
+++ juegos.js
@@ -0,0 +1,32 @@
+const juegos = [
+    {
+        nombre: "gloomhaven",
+        precio: 800,
+        stock: 0,
+    },
+    {
+        nombre: "munchkin",
+        precio: 700,
+        stock: 2,
+    },
+    {
+        nombre: "no lo testeamos ni un poco",
+        precio: 500,
+        stock: 3,
+    },
+    {
+        nombre: "dungeons and drinks",
+        precio: 600,
+        stock: 3,
+    },
+    {
+        nombre: "jumanji",
+        precio: 800,
+        stock: 1,
+    },
+    {
+        nombre: "here to slay",
+        precio: 600,
+        stock: 2,
+    }
+]
